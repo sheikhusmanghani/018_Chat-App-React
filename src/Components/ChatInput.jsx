2f09d336c719570d130f7ead3252288552ef4e12
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2024-11-16 22:35:12 +0500

    msg send to firestore

--- src/Components/ChatInput.jsx
+++ src/Components/ChatInput.jsx
@@ -1,15 +1,29 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { VscSend } from "react-icons/vsc"; // send icon
 import { auth, db } from "../Firebase";
-import { useContext } from "react"; 
+import { useContext } from "react";
+import { context } from "../Context/Context";
 
 const ChatInput = () => {
+  const { currentUser, msgReceiver } = useContext(context);
+  // console.log(">>>", currentUser.uid, msgReceiver.userId); // ok
   //
   const sendMsg = async (e) => {
-    //
     e.preventDefault();
     try {
       const message = e.target.elements.message.value;
+      e.target.reset(); // input clean
+
+      // if (!message || message === " ") return; // to ignore on false or empty message
+
+      // send message
+      const res = await addDoc(collection(db, "messages"), {
+        text: message,
+        sentAt: serverTimestamp(),
+        senderUid: currentUser.uid,
+        receiverUid: msgReceiver.userId,
+      });
+      console.log(res);
     } catch (e) {
       console.log(e.message);
     }
