b363478ee2fc8e4b478bc62fb04e068b0bf95efa
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2024-11-21 17:48:24 +0500

    set current user name . ok

--- src/Chat App/AllChats.jsx
+++ src/Chat App/AllChats.jsx
@@ -1,11 +1,24 @@
 import { useContext, useEffect, useState } from "react";
 import { context } from "../Context/Context";
 import Loader from "../Components/Loader";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../Firebase";
 
 const AllChats = () => {
   const { users, loading, setMsgReceiver, msgReceiver, currentUser } =
     useContext(context);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loggedinUserData, setLoggedinUserData] = useState({});
+
+  // get data of the current user , called in useffect
+  async function getCurrentUserData() {
+    const docRef = doc(db, "users", currentUser.uid);
+    const docSnap = await getDoc(docRef);
+    setLoggedinUserData(docSnap.data());
+  }
+  useEffect(() => {
+    getCurrentUserData();
+  }, []);
 
   // Filter users
   const filteredUsers = users?.filter((user) =>
@@ -25,9 +38,14 @@ const AllChats = () => {
   }
 
   return (
-    <div className="text-black w-full h-full mx-2 md:px-2 md:m-0 flex items-center justify-center md:block">
+    <div className="text-black w-full h-full mx-2 md:m-0 flex items-center justify-center md:block">
+      {/* Navbar for Logged-in User   */}
+      <div className="logged-in-user-navbar w-full py-4 mb-1 hidden md:flex md:justify-center bg-white text-black text-xl uppercase font-bold shadow-2xl">
+        {loggedinUserData.name}
+      </div>
+
       {/* Search Box */}
-      <div className="w-full py-3 hidden md:flex md:justify-center">
+      <div className="w-full pt-1 pb-3 hidden md:flex md:justify-center">
         <input
           type="text"
           placeholder="Search By User Name"
@@ -45,7 +63,7 @@ const AllChats = () => {
               key={user.userId}
               className={`rounded p-2 shadow m-1  cursor-pointer capitalize text-center ${
                 msgReceiver?.userId === user.userId
-                  ? "bg-[#930DA5] text-white" // Selected user
+                  ? "bg-[#930DA5] text-white font-bold " // Selected user
                   : "bg-white text-black font-bold hover:bg-[#e2dfdf]" // Default user
               }`}
               onClick={() => setMsgReceiver(user)}
--- src/Chat App/ChatappWrapper.jsx
+++ src/Chat App/ChatappWrapper.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
 import AllChats from "./AllChats";
 import CurrentChat from "./CurrentChat";
+import { context } from "../Context/Context";
 
 const ChatappWrapper = () => {
+  const { currentUser } = useContext(context);
+  // console.log(currentUser.uid);// ok
+
   return (
     <div className="w-[80vw] h-[85vh] grid grid-rows-5 md:grid-rows-1 md:grid-cols-3  my-3 border rounded-md overflow-hidden">
       <div className="allChats row-span-1 md:col-span-1 overflow-auto ">
